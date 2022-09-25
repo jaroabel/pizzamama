7691386aa27969e8f67beecdcdd0131f6579333f
jaroabel <jaroabel@gmail.com>
2022-09-25 10:08:54 -0400

    first commit

---
+++ pizzamama/menu/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+
+
+# Create your views here.
+# Les pizzas : Végétarienne, Poulet champignons, 4 fromages, Carnivore
+# / menu
+def index(request):
+    '''pizzas = Pizza.objects.all()
+    pizzas_name_and_price = [pizza.nom + " : " + str(pizza.prix) + "$" for pizza in pizzas]
+
+    pizzas_name_and_price_str = ", ".join(pizzas_name_and_price)
+    return HttpResponse("Les pizzas : " + pizzas_name_and_price_str)'''
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
